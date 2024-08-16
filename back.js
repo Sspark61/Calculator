d95981a2bbd62b44928346afd5662108ec1320f4
Ahmed Refat <ahmedrefatabosaif@gmail.com>
2024-08-16 18:11:50 +0300

    calculator is fully operating but without decimals

--- back.js
+++ back.js
@@ -7,6 +7,8 @@ let digits = document.querySelectorAll("#numbers");
 let ops = document.querySelectorAll("#operator");
 let allclear = document.querySelector("#clear");
 let equal = document.querySelector("#equal");
+let dot = document.querySelector("#dot");
+let point = true;
 
 function add(dgt1, dgt2) {
   sum = dgt1 + dgt2;
@@ -28,8 +30,118 @@ function divide(dgt1, dgt2) {
   return rem;
 }
 
-function operate(str){
-    
+function operate(str) {
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "x") {
+      let left = i - 1,
+        right = i + 1,
+        val1 = 0,
+        val2 = 0,
+        factor = 1;
+
+      while (str[left] !== "+" && str[left] !== "-" && left !== -1) {
+        val1 += str[left] * factor;
+        factor *= 10;
+        left--;
+      }
+      while (
+        str[right] !== "+" &&
+        str[right] !== "-" &&
+        str[right] !== "/" &&
+        str[right] !== "x" &&
+        right !== str.length
+      ) {
+        val2 += str[right] * 1;
+        val2 *= 10;
+        right++;
+      }
+      val2 /= 10;
+      let ans = mult(val1, val2);
+      str = str.replace(`${val1}x${val2}`, `${ans}`);
+      i = 0;
+    } else if (str[i] === "/") {
+      let left = i - 1,
+        right = i + 1;
+      let val1 = 0,
+        val2 = 0,
+        factor = 1;
+
+      while (str[left] !== "+" && str[left] !== "-" && left !== -1) {
+        val1 += str[left] * factor;
+        factor *= 10;
+        left--;
+      }
+
+      while (
+        str[right] !== "+" &&
+        str[right] !== "-" &&
+        str[right] !== "x" &&
+        str[right] !== "/" &&
+        right !== str.length
+      ) {
+        val2 += str[right] * 1;
+        val2 *= 10;
+        right++;
+      }
+      val2 /= 10;
+      let ans = divide(val1, val2);
+      str = str.replace(`${val1}/${val2}`, `${ans}`);
+      i = 0;
+    }
+  }
+  console.log(str);
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "+") {
+      let left = i - 1,
+        right = i + 1;
+      let val1 = 0,
+        val2 = 0,
+        factor = 1;
+
+      while (left !== -1) {
+        val1 += str[left] * factor;
+        factor *= 10;
+        left--;
+      }
+      console.log(val1);
+      while (str[right] !== "+" && str[right] !== "-" && right !== str.length) {
+        val2 += str[right] * 1;
+        val2 *= 10;
+        right++;
+      }
+      val2 /= 10;
+      console.log(val2);
+      let ans = add(val1, val2);
+      str = str.replace(`${val1}+${val2}`, `${ans}`);
+      i = 0;
+      console.log(str);
+    } else if (str[i] === "-") {
+      let left = i - 1,
+        right = i + 1;
+      let val1 = 0,
+        val2 = 0,
+        factor = 1;
+
+      while (left !== -1) {
+        val1 += str[left] * factor;
+        factor *= 10;
+        left--;
+      }
+      console.log(val1);
+      while (str[right] !== "+" && str[right] !== "-" && right !== str.length) {
+        val2 += str[right] * 1;
+        val2 *= 10;
+        right++;
+      }
+      val2 /= 10;
+      console.log(val2);
+      let ans = subtract(val1, val2);
+      str = str.replace(`${val1}-${val2}`, `${ans}`);
+      i = 0;
+      console.log(str);
+    }
+  }
+  return str;
 }
 
 allclear.addEventListener("click", () => {
@@ -37,6 +149,7 @@ allclear.addEventListener("click", () => {
   total = "";
   display.innerHTML = equation;
   result.innerHTML = total;
+  point = true;
 });
 
 backspace.addEventListener("click", () => {
@@ -51,14 +164,29 @@ digits.forEach((digit) => {
   });
 });
 
+dot.addEventListener("click", () => {
+  if (point) {
+    equation += dot.innerHTML;
+    display.innerHTML = equation;
+    point = false;
+  }
+});
+
 ops.forEach((op) => {
   op.addEventListener("click", () => {
     equation += op.innerHTML;
     display.innerHTML = equation;
+    point = true;
   });
 });
 
-equal.addEventListener("click" , () => {
-    let ans = operate(equation);
+equal.addEventListener("click", () => {
+  let ans = operate(equation);
+  ans = Number(ans);
+  if (ans === Math.floor(ans)) {
     result.innerHTML = ans;
-})
+  } else {
+    result.innerHTML = ans.toFixed(6);
+  }
+  point = true;
+});
